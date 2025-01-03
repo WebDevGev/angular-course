37c6f19b451ce8f31f6935d075a58c9791836778
Gevorg MESROPIAN <mesropian.g@sfeir.com>
2025-01-03 08:27:40 +0100

    creating the routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ProfileCardComponent } from './common-ui/profile-card/profile-card.component';
-import { ProfileService } from './data/services/profile.service';
-import { Profile } from './data/interfaces/profile.interface';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProfileCardComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  profileService = inject(ProfileService);
-  profiles: Profile[] = [];
-
-  constructor() {
-    this.profileService.getTestAccounts().subscribe((val) => {
-      console.log('Données reçues :', val);
-      this.profiles = val;
-    });
-  }
-}
+export class AppComponent {}
