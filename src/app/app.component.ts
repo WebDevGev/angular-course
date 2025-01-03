829160dcfebead93118101ceec49adee3124e156
Gevorg MESROPIAN <mesropian.g@sfeir.com>
2025-01-03 07:19:07 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProfileCardComponent } from './common-ui/profile-card/profile-card.component';
+import { ProfileService } from './data/services/profile.service';
+import { Profile } from './data/interfaces/profile.interface';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProfileCardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-course';
+  profileService = inject(ProfileService);
+  profiles: Profile[] = [];
+
+  constructor() {
+    this.profileService.getTestAccounts().subscribe((val) => {
+      console.log('Données reçues :', val);
+      this.profiles = val;
+    });
+  }
 }
---
+++ src/app/app.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  imports: [BrowserModule, AppComponent, CommonModule],
+  providers: [],
+})
+export class AppModule {}
